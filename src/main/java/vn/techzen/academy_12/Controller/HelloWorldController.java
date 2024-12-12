8a186a5ed9e970d5b1e07fea444601df62e6f2c3
Hoàng <hoanghung20030@gmail.com>
2024-12-12 21:50:00 +0700

    update api greeting

--- src/main/java/vn/techzen/academy_12/Controller/HelloWorldController.java
+++ src/main/java/vn/techzen/academy_12/Controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package vn.techzen.academy_12.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(defaultValue = "") String name) {
-        return String.format("Hello  %s!!! " ,name);
+    public ResponseEntity<String> greeting(@RequestParam(defaultValue = "") String name) {
+        String message = String.format("Hello %s!!!", name.isEmpty() ? "" : name);
+        return ResponseEntity.ok(message);
     }
-
 }
