da95eafc04c71332ffe1d6bfae2e12fb35b0d799
Hoàng <hoanghung20030@gmail.com>
2024-12-12 22:39:01 +0700

    update cross origin

--- src/main/java/vn/techzen/academy_12/Controller/CalculatorController.java
+++ src/main/java/vn/techzen/academy_12/Controller/CalculatorController.java
@@ -1,10 +1,12 @@
 package vn.techzen.academy_12.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class CalculatorController {
     @GetMapping("/calculator")
